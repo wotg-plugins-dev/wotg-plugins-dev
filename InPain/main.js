4b81d6e3dace486e1c2771dc5ed92df0a5109fdf
Sandrem <oogoeroo@gmail.com>
2014-07-26 16:10:04 +0300

    InPain - Final
    Plugin that changes name of button

---
+++ InPain/main.js
@@ -0,0 +1,7 @@
+﻿Wotg_Plugins.get().addSimplePlugin('InPain', '0.2.2', function (api) {
+
+﻿	api.events.add('beforeLaunch', function () {
+		Wotg.controller().lang.set({'hangar.battleButton' : 'В Боль!'});
+﻿	});
+
+});
