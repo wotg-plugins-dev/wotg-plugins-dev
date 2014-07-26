30fbf69f30d4ace3f24cb82493ce6bcb165403e0
Sandrem <oogoeroo@gmail.com>
2014-07-26 21:01:39 +0300

    Update main.js

--- KISS/main.js
+++ KISS/main.js
@@ -1,17 +1,21 @@
 ﻿Wotg_Plugins.get().addSimplePlugin('KISS', '0.2.2', function (api) {
 
 ﻿	api.events.add('beforeLaunch', function () {
-		console.log("KISS 2!!!")
+		console.log("KISS 3!!!")
 		
 		//SET SIZE
 		//Art
-		Wotg.Card.Markup.Big.markup.children[0].rect = [25, 85, 347, 290]
+		Wotg.Card.Markup.Big.markup.children[0].rect = [25, 85, 347, 290];
 		//Type
-		Wotg.Card.Markup.Big.markup.children[11].rect = [75, 25, 51, 61]
+		Wotg.Card.Markup.Big.markup.children[11].rect = [75, 25, 51, 61];
 		//Title
-		Wotg.Card.Markup.Big.markup.children[5].rect = [125, 28, 228, 30]
+		Wotg.Card.Markup.Big.markup.children[5].rect = [125, 28, 228, 30];
 		//Description
-		Wotg.Card.Markup.Big.markup.children[6].rect = [125, 59, 241, 22]
+		Wotg.Card.Markup.Big.markup.children[6].rect = [125, 59, 241, 22];
+		//Power
+		Wotg.Card.Markup.Big.markup.children[14].rect = [19, 385, 75, 75];
+		//Toughness
+		Wotg.Card.Markup.Big.markup.children[15].rect = [25, 465, 61, 75];		
 
 ﻿	});
 
