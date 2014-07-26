653b4f39f1d3db10520ffc57f6387bd4191790ec
Sandrem <oogoeroo@gmail.com>
2014-07-27 00:35:05 +0300

    Update main.js

--- KISS/main.js
+++ KISS/main.js
@@ -23,6 +23,7 @@
 		preload: function (onComplete) {
 			//Replace Frame
 			Wotg.Images.PreLoader.list['dava-card'] = 'http://s7.hostingkartinok.com/uploads/images/2014/07/ad409f6fa1c6466dd5701fbb6422c30c.png';
+			//Replace Icons
 			Wotg.Images.PreLoader.list['dava-card-icons'] = 'http://s7.hostingkartinok.com/uploads/images/2014/07/331909e2d0461ea9e42f71e717fe4c79.png';
 			return this.preloader = this.makePreloader( Wotg.Images.PreLoader.list, onComplete );
 		},
