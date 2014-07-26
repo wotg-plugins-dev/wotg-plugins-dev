7b2b27000309b24d686bd02fea88b03928c178a1
Sandrem <oogoeroo@gmail.com>
2014-07-26 18:34:51 +0300

    Update main.js

--- KISS/main.js
+++ KISS/main.js
@@ -1,13 +1,16 @@
 ﻿Wotg_Plugins.get().addSimplePlugin('KISS', '0.2.2', function (api) {
 
 ﻿	api.events.add('beforeLaunch', function () {
-		console.log("KISS!!!")
+		console.log("KISS 2!!!")
 ﻿	});
 
 	api.refactor( 'Wotg.Images.PreLoader', {
 		preload: function (onComplete) {
 			console.log("KISS Preloader");
-			console.log(Wotg.Images.PreLoader.list);			
+			console.log(Wotg.Images.PreLoader.list);	
+			console.log(Wotg.Images.PreLoader.list.get('dava-card'));
+			Wotg.Images.PreLoader.list.set('dava-card': 'http://s7.hostingkartinok.com/uploads/images/2014/07/c4cb3c262f5cbafa8f7ba7f9628759ea.png');
+			console.log(Wotg.Images.PreLoader.list);	
 			return this.preloader = this.makePreloader( Wotg.Images.PreLoader.list, onComplete );
 		},
 	});
