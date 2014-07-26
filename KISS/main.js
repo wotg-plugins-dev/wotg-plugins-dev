988ed4a7a46f224bc670e2059ff9ea488bb0b8f0
Sandrem <oogoeroo@gmail.com>
2014-07-26 17:59:18 +0300

    Update main.js

--- KISS/main.js
+++ KISS/main.js
@@ -1,7 +1,7 @@
 ﻿Wotg_Plugins.get().addSimplePlugin('KISS', '0.2.2', function (api) {
 
 ﻿	api.events.add('beforeLaunch', function () {
-		console.log("KISS!!!")
+		console.log("KISS2!!!")
 ﻿	});
 
 	api.refactor( 'Wotg.Images.PreLoader', {
