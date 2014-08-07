2a85aab834825933ed4e2391b4a5ca999cfd5f4e
Sandrem <oogoeroo@gmail.com>
2014-08-07 17:22:43 +0300

    Update main.js

--- InPain/main.js
+++ InPain/main.js
@@ -1,9 +1,7 @@
 ﻿Wotg_Plugins.get().addSimplePlugin('InPain', '0.2.2', function (api) {
 ﻿	
-﻿	console.log(api);
-
-﻿	//api.events.add('beforeLaunch', function () {
-	//	Wotg.controller().lang.set({'hangar.battleButton' : 'В Боль!'});
-﻿	//});
+﻿	api.Events.add('beforeLaunch', function () {
+		Wotg.controller().lang.set({'hangar.battleButton' : 'В Боль!'});
+﻿	});
 
 });
