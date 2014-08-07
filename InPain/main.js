648f8bc88baafbb2cd4b7605ef81dbc1ef14454c
Sandrem <oogoeroo@gmail.com>
2014-08-07 17:13:10 +0300

    Update main.js

--- InPain/main.js
+++ InPain/main.js
@@ -1,4 +1,6 @@
 ﻿Wotg_Plugins.get().addSimplePlugin('InPain', '0.2.2', function (api) {
+﻿	
+﻿	console.log(api);
 
 ﻿	api.events.add('beforeLaunch', function () {
 		Wotg.controller().lang.set({'hangar.battleButton' : 'В Боль!'});
