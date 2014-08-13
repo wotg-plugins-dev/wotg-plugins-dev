d4bdf0a9588055ea8bf5e5ed72876ccc4c8bc467
PavelPonomarenko <shocksilien@gmail.com>
2014-08-13 20:20:59 +0300

    Update repository.js

--- repository.js
+++ repository.js
@@ -1,19 +1,8 @@
-if (window.Wotg_Plugins) {
-	Wotg_Plugins.get().addRepository({
-		title: 'wotg-plugins-dev',
-		plugins: [
-			'TestPlugin',
-			'Katusha',
-			'InPain',
-		]
-	});
-} else {
-	new Wotg.Plugins.Repository({
-		title: 'wotg-plugins-dev',
-		plugins: [
-			'TestPlugin',
-			'Katusha',
-			'InPain'
-		]
-	});
-}
+new Wotg.Plugins.Repository({
+	title: 'wotg-plugins-dev',
+	plugins: [
+		'TestPlugin',
+		'Katusha',
+		'InPain'
+	]
+});
