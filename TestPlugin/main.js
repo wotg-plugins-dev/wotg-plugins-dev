98ae6b45b86704bfce3035ad37d9d4a5e5fe7094
PavelPonomarenko <shocksilien@gmail.com>
2014-08-13 17:25:25 +0300

    Update main.js

--- TestPlugin/main.js
+++ TestPlugin/main.js
@@ -1,4 +1,4 @@
-if (window.Wotg_Plugins) {
+if (Wotg_Plugins) {
 	Wotg_Plugins.get().addSimplePlugin('TestPlugin', '0.2.2', function (api) {
 		console.log('TestPlugin 0.2.2', { Wotg: Wotg, api: api, atom: atom });
 	});	
