d9ec58a6e8042481ed7f0753dfeae1176851be5d
PavelPonomarenko <shocksilien@gmail.com>
2014-08-13 15:43:45 +0300

    Update main.js

--- TestPlugin/main.js
+++ TestPlugin/main.js
@@ -1,3 +1,16 @@
-Wotg_Plugins.get().addSimplePlugin('TestPlugin', '0.2.2', function (api) {
-	console.log('TestPlugin 0.2.2', { Wotg: Wotg, api: api, atom: atom });
-});
+if (Wotg_Plugins) {
+	Wotg_Plugins.get().addSimplePlugin('TestPlugin', '0.2.2', function (api) {
+		console.log('TestPlugin 0.2.2', { Wotg: Wotg, api: api, atom: atom });
+	});	
+} else {
+	new Wotg.Plugins.Simple({
+		title  : 'MyPluginTitle',
+		version: '0.2.3'
+	}, function (plugin, events) {
+		console.log('MyPluginTitle', { Wotg: Wotg, plugin: plugin, atom: atom });
+	
+		events.add('initialize', function () {
+			console.log('MyPluginTitle initialized');
+		});
+	});
+}
