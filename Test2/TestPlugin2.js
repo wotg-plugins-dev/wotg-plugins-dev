8ae648e6194b80c0eb3081fed818cf9334ca420b
Isk1n <Isk1n@users.noreply.github.com>
2014-08-17 11:49:38 +0300

    Revert "detete test plugin"
    This reverts commit 8d4279be9bcb755db266e3a1845e263d3240ca94.

---
+++ Test2/TestPlugin2.js
@@ -0,0 +1,19 @@
+new Wotg.Plugins.Simple({
+	title  : 'TestPlugin',
+	version: '0.2.3'
+}, function (plugin, events) {
+	plugin.addImagesPreload({
+		'test': 'image.png'
+	});
+	
+	console.log('TestPlugin', { Wotg: Wotg, plugin: plugin, atom: atom });
+
+	events.add('initialize', function () {
+		console.log('TestPlugin initialized');
+	});
+
+	events.add('afterLaunch', function () {
+		atom.dom(plugin.getImage('test')).appendTo('body');
+		console.log(plugin.getImage('test'));
+	});
+});
