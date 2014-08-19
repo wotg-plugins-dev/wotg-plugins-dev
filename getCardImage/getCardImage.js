e4f6297411ea7fc05e289c6827b4e4cd83107d5d
Isk1n <Isk1n@users.noreply.github.com>
2014-08-19 11:07:00 +0300

    Debug

--- getCardImage/getCardImage.js
+++ getCardImage/getCardImage.js
@@ -418,6 +418,7 @@ new Wotg.Plugins.Simple({
                 for (var i = 0; i < cards.length; i++) {
 
                     var proto = controller.protos.get(cards[i]);
+                    debugger;
                     var model = new Wotg.Card.Models.Model(proto);
                     try {
                         var view = new Wotg.Card.Views.Big(model);
