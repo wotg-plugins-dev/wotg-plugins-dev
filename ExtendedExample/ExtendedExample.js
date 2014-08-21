b071f1c0d55222af837424740db559451de30e85
bzz86 <bzz86@mail.ru>
2014-08-22 01:01:51 +0200

    Revert "temporary commit to remove plugin"
    This reverts commit d60644310fa9ed622bdece79f343984f876b336f.

--- ExtendedExample/ExtendedExample.js
+++ ExtendedExample/ExtendedExample.js
@@ -2,5 +2,57 @@ new Wotg.Plugins.Simple({
 	title  : 'ExtendedExample',
 	version: '0.2.3'
 }, function (plugin, events) {
-
+	plugin.addImages({
+		'added-image': 'image.png'
+	});
+	
+	plugin.replaceImages({
+		'replaced-image': 'image.png'
+	});
+	
+	plugin.replaceCardImages([
+		'gh_trainingslager',
+		'uh_trainingcamp',
+		'sh_uchebnayachast',
+		
+		'go_kameradderarbeit',
+		'go_tagderwehrmacht',
+		'gp_auskundschaftersderpdmuncheberg',
+		'gv_leichttraktor',
+		'gv_panzerjagerI',
+		'gv_pzb2',
+		'gv_t25',
+		
+		'uo_crushprussian',
+		'uv_ramII',
+		'uv_t1lt',
+		'uv_t14',
+		'uv_t18',
+		
+		'so_budbditelnym',
+		'so_takbilotakbudet',
+		'so_udarmolota',
+		'sp_strelkiuzhnogofronta',
+		'sv_kv220',
+		'sv_matilda4',
+		'sv_ms1',
+		'sv_su18'
+	]);
+	
+	plugin.replaceAudio([
+		'background'
+	]);
+	
+	plugin.markupChange(Wotg.Card.Markup.HqBig)
+		.move('Power'    , [  0, 97 ])
+		.move('Toughness', [ 70, 20 ]);
+	
+	plugin.markupChange(Wotg.Card.Markup.Big)
+		.move('Power'    , [  0, 97 ])
+		.move('Toughness', [ 70, 20 ]);
+	
+	events.add('afterLaunch', function () {
+		console.log(plugin.getImage('added-image'));
+		console.log(Wotg.controller().images.get('replaced-image'));
+	});
 });
