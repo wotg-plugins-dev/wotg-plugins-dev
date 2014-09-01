0f8f78fbd05ef237b880a51ed1d9375c972cd2e2
Isk1n <Isk1n@users.noreply.github.com>
2014-09-01 15:10:06 +0300

    Move css from function, move JQTC init on afterLaunch

--- ConsoleAutocomplete/ConsoleAutocomplete.js
+++ ConsoleAutocomplete/ConsoleAutocomplete.js
@@ -3,62 +3,48 @@ new Wotg.Plugins.Simple({
     version: '0.2.4'
 }, function(plugin, events) {
     var pluginPath = 'https://' + (plugin.repository || 'wotg-plugins-dev') + '.github.io/wotg-plugins-dev/' + plugin.title + '/';
-
-    /*===========================
-    =            CSS            =
-    ===========================*/
-
-    function CSSInit() {
-        var css = document.createElement('link');
-        css.rel = 'stylesheet';
-        css.href = pluginPath + 'ConsoleAutocomplete.css';
-        document.head.appendChild(css);
-    }
+    var commands = [];
 
     /*==================================================================================
 	=            JQuery Textcomplete http://yuku-t.com/jquery-textcomplete/            =
 	==================================================================================*/
 
-    function JQTxtCmpltInit(callback) {
+    function JQTxtCmpltInit() {
         var JQTxtCmplt = document.createElement('script');
         JQTxtCmplt.src = pluginPath + 'jquery.textcomplete.js';
         document.body.appendChild(JQTxtCmplt);
-        JQTxtCmplt.onload = callback;
     }
 
-    /*==============================
-    =            Events            =
-    ==============================*/
-
     events.add('initialize', function() {
-        CSSInit();
+        var css = document.createElement('link');
+        css.rel = 'stylesheet';
+        css.href = pluginPath + 'ConsoleAutocomplete.css';
+        document.head.appendChild(css);
         console.log(plugin.title + ' version ' + plugin.version + ' from ' + plugin.repository + ' initialized');
     });
 
-    atom.Keyboard().events.add('gravis', function() {
-        JQTxtCmpltInit(function() {
-            // Make array of available commands
-            var commands = [];
-            var commandsObj = Wotg.Utils.Console().commands;
-            for (command in commandsObj) {
-                if (!commandsObj.hasOwnProperty(command)) continue;
-                commands.push(command);
-            }
-
-            $('.console-input').textcomplete([{ // tech companies
-                match: /\b(\w{1,})$/,
-                search: function(term, callback) {
-                    callback($.map(commands, function(word) {
-                        return word.indexOf(term) === 0 ? word : null;
-                    }));
-                },
-                index: 1,
-                replace: function(word) {
-                    return word + ' ';
-                }
-            }]);
-        });
-
+    events.add('afterLaunch', function() {
+        JQTxtCmpltInit();
+        // Make array of available commands
+        var commandsObj = Wotg.Utils.Console().commands;
+        for (command in commandsObj) {
+            if (!commandsObj.hasOwnProperty(command)) continue;
+            commands.push(command);
+        }
     });
 
+    atom.Keyboard().events.add('gravis', function() {
+        $('.console-input').textcomplete([{
+            match: /\b(\w{1,})$/,
+            search: function(term, callback) {
+                callback($.map(commands, function(word) {
+                    return word.indexOf(term) === 0 ? word : null;
+                }));
+            },
+            index: 1,
+            replace: function(word) {
+                return word + ' ';
+            }
+        }]);
+    });
 });
