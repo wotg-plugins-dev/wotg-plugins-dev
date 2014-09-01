06d7591ea4ed42769eb48341af35cb4cc2f8d958
Isk1n <Isk1n@users.noreply.github.com>
2014-09-01 09:05:57 +0300

    Create ConsoleAutocomplete

---
+++ ConsoleAutocomplete/ConsoleAutocomplete.js
@@ -0,0 +1,8 @@
+new Wotg.Plugins.Simple({
+    title: 'ConsoleAutocomplete',
+    version: '0.2.4'
+}, function(plugin, events) {
+    events.add('aterLaunch', function() {
+        debugger;
+    });
+});
