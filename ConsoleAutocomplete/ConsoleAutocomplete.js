72a669b66defac97c03240f9bcd778096f8ee529
Isk1n <Isk1n@users.noreply.github.com>
2014-09-01 09:52:55 +0300

    Include JQuery Textcomplete

--- ConsoleAutocomplete/ConsoleAutocomplete.js
+++ ConsoleAutocomplete/ConsoleAutocomplete.js
@@ -2,11 +2,34 @@ new Wotg.Plugins.Simple({
     title: 'ConsoleAutocomplete',
     version: '0.2.4'
 }, function(plugin, events) {
+    /*==================================================================================
+	=            JQuery Textcomplete http://yuku-t.com/jquery-textcomplete/            =
+	==================================================================================*/
+    var JQTxtCmplt = document.createElement('script');
+    JQTxtCmplt.src = '//cdnjs.cloudflare.com/ajax/libs/jquery.textcomplete/0.2.2/jquery.textcomplete.min.js';
+    document.body.appendChild(JQTxtCmplt);
+
     events.add('afterLaunch', function() {
-        var commands = Wotg.Utils.Console().commands;
-        for (command in commands) {
-            if (!commands.hasOwnProperty(command)) continue;
-            console.log(command);
+        // Make array of available commands
+        var commands = [];
+        var commandsObj = Wotg.Utils.Console().commands;
+        for (command in commandsObj) {
+            if (!commandsObj.hasOwnProperty(command)) continue;
+            commands.append(command);
         }
+
+        $('.console-input').textcomplete([{ // tech companies
+            match: /\b(\w{1,})$/,
+            search: function(term, callback) {
+                callback($.map(commands, function(word) {
+                    return word.indexOf(term) === 0 ? word : null;
+                }));
+            },
+            index: 1,
+            replace: function(word) {
+                return word + ' ';
+            }
+        }]);
+        debugger
     });
 });
