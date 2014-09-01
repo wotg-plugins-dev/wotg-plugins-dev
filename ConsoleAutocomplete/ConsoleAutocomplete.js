c40f0e13fb639964fa04a407cb25e962ad3c4ea5
Isk1n <Isk1n@users.noreply.github.com>
2014-09-01 09:18:13 +0300

    Fix ConsoleAutocomplete

--- ConsoleAutocomplete/ConsoleAutocomplete.js
+++ ConsoleAutocomplete/ConsoleAutocomplete.js
@@ -2,7 +2,7 @@ new Wotg.Plugins.Simple({
     title: 'ConsoleAutocomplete',
     version: '0.2.4'
 }, function(plugin, events) {
-    events.add('aterLaunch', function() {
-        console.log(Wotg.Utils);
+    events.add('afterLaunch', function() {
+        debugger
     });
 });
