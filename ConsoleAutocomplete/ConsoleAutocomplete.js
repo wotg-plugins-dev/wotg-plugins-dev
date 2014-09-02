1647c3e9baa9b81bde739da2d53dd84dd26d06e4
Isk1n <Isk1n@users.noreply.github.com>
2014-09-02 20:29:30 +0300

    Rename conIn, add exec object.object.object

--- ConsoleAutocomplete/ConsoleAutocomplete.js
+++ ConsoleAutocomplete/ConsoleAutocomplete.js
@@ -40,8 +40,9 @@ new Wotg.Plugins.Simple({
     });
 
     atom.Keyboard().events.add('gravis', function() {
+        var conIn = $('.console-input');
         // Console command
-        $('.console-input').textcomplete([{
+        conIn.textcomplete([{
             match: /^(\w{0,})$/,
             search: function(term, callback) {
                 callback($.map(commands, function(word) {
@@ -54,7 +55,7 @@ new Wotg.Plugins.Simple({
             }
         }]);
         // man
-        $('.console-input').textcomplete([{
+        conIn.textcomplete([{
             match: /^man (\w{0,})$/,
             search: function(term, callback) {
                 callback($.map(commands, function(word) {
@@ -67,7 +68,7 @@ new Wotg.Plugins.Simple({
             }
         }]);
         // plugins
-        $('.console-input').textcomplete([{
+        conIn.textcomplete([{
             match: /^plugins (\w{0,})$/,
             search: function(term, callback) {
                 callback($.map(pluginsCommands, function(word) {
@@ -83,7 +84,7 @@ new Wotg.Plugins.Simple({
         ['rm', 'config', 'code'].forEach(function(operation) {
             var re = new RegExp('^plugins ' + operation + ' (\\w{0,})$');
 
-            $('.console-input').textcomplete([{
+            conIn.textcomplete([{
                 match: re,
                 search: function(term, callback) {
                     callback($.map(Wotg.Plugins.get().installer.connectedPlugins, function(word) {
@@ -97,7 +98,7 @@ new Wotg.Plugins.Simple({
             }]);
         });
         // exec
-        $('.console-input').textcomplete([{
+        conIn.textcomplete([{
             match: /^exec (\w{0,})$/,
             search: function(term, callback) {
                 callback($.map(Object.keys(window), function(word) {
@@ -110,7 +111,36 @@ new Wotg.Plugins.Simple({
             }
         }]);
 
-        $('.console-input').on('focus', function() {
+        // exec object.object.object
+        var dots = /(^exec )(.*)(\.)(\w{0,})$/;
+        conIn.textcomplete([{
+            match: dots,
+            search: function(term, callback) {
+                var tree = dots.exec(conIn[0].value)[2].split('.');
+                var leaf = tree.reduce(function(obj, parameter) {
+                    return obj[parameter];
+                }, window);
+                if (!(leaf instanceof Object)) return
+                callback($.map( Object.keys(leaf).sort() , function(word) {
+                    return word.indexOf(term) === 0 ? word : null;
+                }));
+            },
+            index: 4,
+            replace: function(word) {
+                return '$1$2$3' + word;
+            }
+        }]);
+
+        conIn.on('focus', function() {
+            var textComplete = $(this).data('textComplete');
+            // Cursor has not set yet. And wait 100ms to skip global click event.
+            setTimeout(function() {
+                // Cursor is ready.
+                textComplete.trigger();
+            }, 100);
+        });
+
+        conIn.on('click', function() {
             var textComplete = $(this).data('textComplete');
             // Cursor has not set yet. And wait 100ms to skip global click event.
             setTimeout(function() {
