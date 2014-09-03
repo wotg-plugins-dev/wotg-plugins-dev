790c8906517e77355c54c66d8f53be7d747b16f9
Isk1n <Isk1n@users.noreply.github.com>
2014-09-03 10:25:40 +0300

    Revert "test method previous apply"
    This reverts commit 46b3e0cf48cf1e3363665b9ba7ae2522863afa7e.

--- NotAPlugin/NotAPlugin.js
+++ NotAPlugin/NotAPlugin.js
@@ -4,7 +4,33 @@ new Wotg.Plugins.Simple({
 }, function(plugin, events) {
     plugin.refactor('Wotg.Utils.Console', {
         initialize: function method() {
-            method.previous.apply(this, arguments);
+            console.log('test');
+            this.bindMethods(['onExec', 'onClear', 'onSubmit', 'onToggle', 'onSet', 'man']);
+
+            this.events = new atom.Events(this);
+            this.wrapper = this.create('div', 'wrapper', 'body');
+            this.inner = this.create('div', 'inner', this.wrapper);
+            this.output = this.create('div', 'output', this.inner);
+            this.input = this.create('textarea', 'input', this.inner);
+
+            this.bindKeyboard();
+
+            this.commands = {};
+            this.constructor.commands.forEach(function(command) {
+                this.register(command);
+            }.bind(this));
+
+            this.storage = this.getStorage();
+            this.currentStep = this.storage.length;
+
+            this.input.first.addEventListener("dragenter", Mouse.prevent, false);
+            this.input.first.addEventListener("dragover", Mouse.prevent, false);
+            this.input.first.addEventListener('drop', function(e) {
+                e.stopPropagation();
+                e.preventDefault();
+                this.toggle();
+                Wotg.controller().replays.read(e.dataTransfer.files[0]);
+            }.bind(this), false);
         },
     });
 });
