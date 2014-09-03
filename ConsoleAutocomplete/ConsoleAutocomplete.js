d8df5e7b8270c50e6876b6940bdcec31e9f2e6a6
Isk1n <Isk1n@users.noreply.github.com>
2014-09-03 09:59:09 +0300

    Correct right re

--- ConsoleAutocomplete/ConsoleAutocomplete.js
+++ ConsoleAutocomplete/ConsoleAutocomplete.js
@@ -71,7 +71,7 @@ new Wotg.Plugins.Simple({
         }]);
         // plugins
         conIn.textcomplete([{
-            match: /^plugins ([\w\:]{0,})$/,
+            match: /^plugins (\w{0,})$/,
             search: function(term, callback) {
                 callback($.map(pluginsCommands, function(word) {
                     return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
@@ -84,7 +84,7 @@ new Wotg.Plugins.Simple({
         }]);
         // plugins (rm || config || code)
         ['rm', 'config', 'code'].forEach(function(operation) {
-            var re = new RegExp('^plugins ' + operation + ' (\\w{0,})$');
+            var re = new RegExp('^plugins ' + operation + ' ([\\w:]{0,})$');
 
             conIn.textcomplete([{
                 match: re,
