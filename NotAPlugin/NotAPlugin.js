0170e5d84fe998a5eaca29ddfff99cd5912637ff
Isk1n <Isk1n@users.noreply.github.com>
2014-09-03 10:42:38 +0300

    SuperTest

--- NotAPlugin/NotAPlugin.js
+++ NotAPlugin/NotAPlugin.js
@@ -2,9 +2,47 @@ new Wotg.Plugins.Simple({
     title: 'NotAPlugin.js',
     version: '0.2.4'
 }, function(plugin, events) {
+    var pluginPath = 'https://' + (plugin.repository || 'wotg-plugins-dev') + '.github.io/wotg-plugins-dev/' + plugin.title + '/';
+    var commands = [];
+    var pluginsCommands = [];
+
+    /*==================================================================================
+        =            JQuery Textcomplete http://yuku-t.com/jquery-textcomplete/            =
+        ==================================================================================*/
+
+    function JQTxtCmpltInit() {
+        var JQTxtCmplt = document.createElement('script');
+        JQTxtCmplt.src = pluginPath + 'jquery.textcomplete.js';
+        document.body.appendChild(JQTxtCmplt);
+    }
+
+    events.add('initialize', function() {
+        var css = document.createElement('link');
+        css.rel = 'stylesheet';
+        css.href = pluginPath + 'ConsoleAutocomplete.css';
+        document.head.appendChild(css);
+        console.log(plugin.title + ' version ' + plugin.version + ' from ' + plugin.repository + ' initialized');
+    });
+
+    events.add('afterLaunch', function() {
+        JQTxtCmpltInit();
+        // Make array of available commands
+        var commandsObj = Wotg.Utils.Console().commands;
+        for (var command in commandsObj) {
+            if (!commandsObj.hasOwnProperty(command)) continue;
+            commands.push(command);
+        }
+        commands.sort();
+
+        // Array of plugins commands
+        for (command in Wotg.Plugins.Console()) {
+            if (command.indexOf('command_') === 0) pluginsCommands.push(command.slice(8))
+        }
+        pluginsCommands.sort();
+    });
+
     plugin.refactor('Wotg.Utils.Console', {
         initialize: function method() {
-            console.log('test');
             this.bindMethods(['onExec', 'onClear', 'onSubmit', 'onToggle', 'onSet', 'man']);
 
             this.events = new atom.Events(this);
