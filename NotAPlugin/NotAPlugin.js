ea72831334bc0442f3b68a0d9509826b88938f24
Isk1n <Isk1n@users.noreply.github.com>
2014-09-03 13:28:57 +0300

    JQCT init on focus

--- NotAPlugin/NotAPlugin.js
+++ NotAPlugin/NotAPlugin.js
@@ -14,6 +14,126 @@ new Wotg.Plugins.Simple({
         var JQTxtCmplt = document.createElement('script');
         JQTxtCmplt.src = pluginPath + 'jquery.textcomplete.js';
         document.body.appendChild(JQTxtCmplt);
+        JQ.onload = function() {
+            makeSuggestions();
+        }
+    }
+
+    function makeSuggestions() {
+        var conIn = $('.console-input');
+        // debugger
+        // Console command
+        conIn.textcomplete([{
+            match: /^(\w{0,})$/,
+            search: function(term, callback) {
+                callback($.map(commands, function(word) {
+                    return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
+                }));
+            },
+            index: 1,
+            replace: function(word) {
+                return word + ' ';
+            }
+        }]);
+        // man
+        conIn.textcomplete([{
+            match: /^man (\w{0,})$/,
+            search: function(term, callback) {
+                callback($.map(commands, function(word) {
+                    return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
+                }));
+            },
+            index: 1,
+            replace: function(word) {
+                return 'man ' + word;
+            }
+        }]);
+        // plugins
+        conIn.textcomplete([{
+            match: /^plugins (\w{0,})$/,
+            search: function(term, callback) {
+                callback($.map(pluginsCommands, function(word) {
+                    return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
+                }));
+            },
+            index: 1,
+            replace: function(word) {
+                return 'plugins ' + word + ' ';
+            }
+        }]);
+        // plugins (rm || config || code)
+        ['rm', 'config', 'code'].forEach(function(operation) {
+            var re = new RegExp('^plugins ' + operation + ' ([\\w:]{0,})$');
+
+            conIn.textcomplete([{
+                match: re,
+                search: function(term, callback) {
+                    callback($.map(Wotg.Plugins.get().installer.connectedPlugins.sort(), function(word) {
+                        return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
+                    }));
+                },
+                index: 1,
+                replace: function(word) {
+                    return 'plugins ' + operation + ' ' + word;
+                }
+            }]);
+        });
+        // exec
+        conIn.textcomplete([{
+            match: /^exec (\w{0,})$/,
+            search: function(term, callback) {
+                callback($.map(Object.keys(window).sort(), function(word) {
+                    return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
+                }));
+            },
+            index: 1,
+            replace: function(word) {
+                return 'exec ' + word;
+            }
+        }]);
+        // exec object.object.object
+        conIn.textcomplete([{
+            match: /(^exec )([^\(]*)(\.)(\w{0,})$/,
+            search: function(term, callback) {
+                var root = /(^exec )([^\(]*)(\.)(\w{0,})$/.exec(conIn[1].value);
+                if (!root) {
+                    callback($.map([], function() {
+                        return null;
+                    }));
+                    return;
+                }
+                var tree = root[2].split('.');
+                var leaf = tree.reduce(function(obj, parameter) {
+                    return obj[parameter];
+                }, window);
+                if (!(leaf instanceof Object)) {
+                    callback($.map([], function() {
+                        return null;
+                    }));
+                    return;
+                }
+                callback($.map(Object.keys(leaf).sort(), function(word) {
+                    return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
+                }));
+            },
+            index: 4,
+            replace: function(word) {
+                return '$1$2$3' + word;
+            }
+        }]);
+        // set
+        conIn.textcomplete([{
+            match: /^set (\w{0,})$/,
+            search: function(term, callback) {
+                callback($.map(['maxLogDepth', 'height', 'margin'], function(word) {
+                    return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
+                }));
+            },
+            index: 1,
+            replace: function(word) {
+                return 'set ' + word + '=';
+            }
+        }]);
     }
 
     events.add('initialize', function() {
@@ -43,10 +163,6 @@ new Wotg.Plugins.Simple({
 
     plugin.refactor('Wotg.Utils.Console', {
         initialize: function method() {
-            /*=====================================
-            =            Vanilla part1            =
-            =====================================*/
-
             this.bindMethods(['onExec', 'onClear', 'onSubmit', 'onToggle', 'onSet', 'man']);
 
             this.events = new atom.Events(this);
@@ -54,132 +170,10 @@ new Wotg.Plugins.Simple({
             this.inner = this.create('div', 'inner', this.wrapper);
             this.output = this.create('div', 'output', this.inner);
             this.input = this.create('textarea', 'input', this.inner).addEvent({
-
-                /*==============================
-                =            Plugin            =
-                ==============================*/
-
                 focus: function() {
-                    var conIn = $('.console-input');
-                    // debugger
-                    // Console command
-                    conIn.textcomplete([{
-                        match: /^(\w{0,})$/,
-                        search: function(term, callback) {
-                            callback($.map(commands, function(word) {
-                                return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
-                            }));
-                        },
-                        index: 1,
-                        replace: function(word) {
-                            return word + ' ';
-                        }
-                    }]);
-                    // man
-                    conIn.textcomplete([{
-                        match: /^man (\w{0,})$/,
-                        search: function(term, callback) {
-                            callback($.map(commands, function(word) {
-                                return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
-                            }));
-                        },
-                        index: 1,
-                        replace: function(word) {
-                            return 'man ' + word;
-                        }
-                    }]);
-                    // plugins
-                    conIn.textcomplete([{
-                        match: /^plugins (\w{0,})$/,
-                        search: function(term, callback) {
-                            callback($.map(pluginsCommands, function(word) {
-                                return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
-                            }));
-                        },
-                        index: 1,
-                        replace: function(word) {
-                            return 'plugins ' + word + ' ';
-                        }
-                    }]);
-                    // plugins (rm || config || code)
-                    ['rm', 'config', 'code'].forEach(function(operation) {
-                        var re = new RegExp('^plugins ' + operation + ' ([\\w:]{0,})$');
-
-                        conIn.textcomplete([{
-                            match: re,
-                            search: function(term, callback) {
-                                callback($.map(Wotg.Plugins.get().installer.connectedPlugins.sort(), function(word) {
-                                    return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
-                                }));
-                            },
-                            index: 1,
-                            replace: function(word) {
-                                return 'plugins ' + operation + ' ' + word;
-                            }
-                        }]);
-                    });
-                    // exec
-                    conIn.textcomplete([{
-                        match: /^exec (\w{0,})$/,
-                        search: function(term, callback) {
-                            callback($.map(Object.keys(window).sort(), function(word) {
-                                return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
-                            }));
-                        },
-                        index: 1,
-                        replace: function(word) {
-                            return 'exec ' + word;
-                        }
-                    }]);
-                    // exec object.object.object
-                    conIn.textcomplete([{
-                        match: /(^exec )([^\(]*)(\.)(\w{0,})$/,
-                        search: function(term, callback) {
-                            var root = /(^exec )([^\(]*)(\.)(\w{0,})$/.exec(conIn[1].value);
-                            if (!root) {
-                                callback($.map([], function() {
-                                    return null;
-                                }));
-                                return;
-                            }
-                            var tree = root[2].split('.');
-                            var leaf = tree.reduce(function(obj, parameter) {
-                                return obj[parameter];
-                            }, window);
-                            if (!(leaf instanceof Object)) {
-                                callback($.map([], function() {
-                                    return null;
-                                }));
-                                return;
-                            }
-                            callback($.map(Object.keys(leaf).sort(), function(word) {
-                                return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
-                            }));
-                        },
-                        index: 4,
-                        replace: function(word) {
-                            return '$1$2$3' + word;
-                        }
-                    }]);
-                    // set
-                    conIn.textcomplete([{
-                        match: /^set (\w{0,})$/,
-                        search: function(term, callback) {
-                            callback($.map(['maxLogDepth', 'height', 'margin'], function(word) {
-                                return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
-                            }));
-                        },
-                        index: 1,
-                        replace: function(word) {
-                            return 'set ' + word + '=';
-                        }
-                    }]);
+                    JQTxtCmpltInit();
                 }
             });
-
-            /*=====================================
-            =            Vanilla part2            =
-            =====================================*/
 
             this.bindKeyboard();
 
