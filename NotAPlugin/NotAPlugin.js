33bd86553d0aa3222b31975054215bebe84e3432
Isk1n <Isk1n@users.noreply.github.com>
2014-09-03 10:25:23 +0300

    Revert "test"
    This reverts commit 3be8535150997e59e5e1f57e0186df1b988f1f35.

--- NotAPlugin/NotAPlugin.js
+++ NotAPlugin/NotAPlugin.js
@@ -4,9 +4,7 @@ new Wotg.Plugins.Simple({
 }, function(plugin, events) {
     plugin.refactor('Wotg.Utils.Console', {
         initialize: function method() {
-            console.log('test');
             method.previous.apply(this, arguments);
-            console.log('test2');
         },
     });
 });
