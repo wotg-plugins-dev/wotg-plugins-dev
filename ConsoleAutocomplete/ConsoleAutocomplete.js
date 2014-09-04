46b39e4d59daf71d3ae45bbf2a55b08e70be2e3b
Isk1n <Isk1n@users.noreply.github.com>
2014-09-04 12:23:16 +0300

    Replace method code with method.previous

--- ConsoleAutocomplete/ConsoleAutocomplete.js
+++ ConsoleAutocomplete/ConsoleAutocomplete.js
@@ -163,40 +163,14 @@ new Wotg.Plugins.Simple({
 
     plugin.refactor('Wotg.Utils.Console', {
         initialize: function method() {
-            this.bindMethods(['onExec', 'onClear', 'onSubmit', 'onToggle', 'onSet', 'man']);
+            method.previous.call(this);
+            this.input.addEvent({
 
-            this.events = new atom.Events(this);
-            this.wrapper = this.create('div', 'wrapper', 'body');
-            this.inner = this.create('div', 'inner', this.wrapper);
-            this.output = this.create('div', 'output', this.inner);
-            this.input = this.create('textarea', 'input', this.inner).addEvent({
-                /*========================================
-                =            New code is here            =
-                ========================================*/
                 focus: function() {
                     makeSuggestions();
                 }
-                /*-----  End of new code  ------*/
+
             });
-
-            this.bindKeyboard();
-
-            this.commands = {};
-            this.constructor.commands.forEach(function(command) {
-                this.register(command);
-            }.bind(this));
-
-            this.storage = this.getStorage();
-            this.currentStep = this.storage.length;
-
-            this.input.first.addEventListener("dragenter", Mouse.prevent, false);
-            this.input.first.addEventListener("dragover", Mouse.prevent, false);
-            this.input.first.addEventListener('drop', function(e) {
-                e.stopPropagation();
-                e.preventDefault();
-                this.toggle();
-                Wotg.controller().replays.read(e.dataTransfer.files[0]);
-            }.bind(this), false);
         },
     });
 });
