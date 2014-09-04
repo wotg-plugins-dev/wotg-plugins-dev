b74ab585bb35f274f0e3671fab3c87b4d04b6c9f
Isk1n <Isk1n@users.noreply.github.com>
2014-09-04 11:37:48 +0300

    Move code from NotAPlugin to ConsoleAutocomplete

--- ConsoleAutocomplete/ConsoleAutocomplete.js
+++ ConsoleAutocomplete/ConsoleAutocomplete.js
@@ -7,41 +7,20 @@ new Wotg.Plugins.Simple({
     var pluginsCommands = [];
 
     /*==================================================================================
-	=            JQuery Textcomplete http://yuku-t.com/jquery-textcomplete/            =
-	==================================================================================*/
+    =            JQuery Textcomplete http://yuku-t.com/jquery-textcomplete/            =
+    ==================================================================================*/
 
-    function JQTxtCmpltInit() {
-        var JQTxtCmplt = document.createElement('script');
-        JQTxtCmplt.src = pluginPath + 'jquery.textcomplete.js';
-        document.body.appendChild(JQTxtCmplt);
+    function jQtcInit() {
+        var jQtcScript = document.createElement('script');
+        jQtcScript.src = pluginPath + 'jquery.textcomplete.js';
+        var jQueryWaiting = setInterval(function() {
+            if (!window.jQuery) return;
+            document.body.appendChild(jQtcScript);
+            clearInterval(jQueryWaiting);
+        }, 500);
     }
 
-    events.add('initialize', function() {
-        var css = document.createElement('link');
-        css.rel = 'stylesheet';
-        css.href = pluginPath + 'ConsoleAutocomplete.css';
-        document.head.appendChild(css);
-        console.log(plugin.title + ' version ' + plugin.version + ' from ' + plugin.repository + ' initialized');
-    });
-
-    events.add('afterLaunch', function() {
-        JQTxtCmpltInit();
-        // Make array of available commands
-        var commandsObj = Wotg.Utils.Console().commands;
-        for (var command in commandsObj) {
-            if (!commandsObj.hasOwnProperty(command)) continue;
-            commands.push(command);
-        }
-        commands.sort();
-
-        // Array of plugins commands
-        for (command in Wotg.Plugins.Console()) {
-            if (command.indexOf('command_') === 0) pluginsCommands.push(command.slice(8))
-        }
-        pluginsCommands.sort();
-    });
-
-    atom.Keyboard().events.add('gravis', function() {
+    function makeSuggestions() {
         var conIn = $('.console-input');
         // Console command
         conIn.textcomplete([{
@@ -112,18 +91,18 @@ new Wotg.Plugins.Simple({
                 return 'exec ' + word;
             }
         }]);
-        // exec object.object.object
+        // exec object.object_.object5
         conIn.textcomplete([{
-            match: /(^exec )([^\(]*)(\.)(\w{0,})$/,
+            match: /(^exec [\w\.]*\.)(\w{0,})$/,
             search: function(term, callback) {
-                var root = /(^exec )([^\(]*)(\.)(\w{0,})$/.exec(conIn[1].value);
+                var root = /^exec ([\w\.]*)\.\w*$/.exec(conIn[1].value);
                 if (!root) {
                     callback($.map([], function() {
                         return null;
                     }));
                     return;
                 }
-                var tree = root[2].split('.');
+                var tree = root[1].split('.');
                 var leaf = tree.reduce(function(obj, parameter) {
                     return obj[parameter];
                 }, window);
@@ -137,9 +116,9 @@ new Wotg.Plugins.Simple({
                     return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
                 }));
             },
-            index: 4,
+            index: 2,
             replace: function(word) {
-                return '$1$2$3' + word;
+                return '$1' + word;
             }
         }]);
         // set
@@ -155,5 +134,69 @@ new Wotg.Plugins.Simple({
                 return 'set ' + word + '=';
             }
         }]);
+    }
+
+    events.add('initialize', function() {
+        var css = document.createElement('link');
+        css.rel = 'stylesheet';
+        css.href = pluginPath + 'ConsoleAutocomplete.css';
+        document.head.appendChild(css);
+        jQtcInit();
+        console.log(plugin.title + ' version ' + plugin.version + ' from ' + plugin.repository + ' initialized');
+    });
+
+    events.add('afterLaunch', function() {
+        // Make array of available commands
+        var commandsObj = Wotg.Utils.Console().commands;
+        for (var command in commandsObj) {
+            if (!commandsObj.hasOwnProperty(command)) continue;
+            commands.push(command);
+        }
+        commands.sort();
+
+        // Array of plugins commands
+        for (command in Wotg.Plugins.Console()) {
+            if (command.indexOf('command_') === 0) pluginsCommands.push(command.slice(8))
+        }
+        pluginsCommands.sort();
+    });
+
+    plugin.refactor('Wotg.Utils.Console', {
+        initialize: function method() {
+            this.bindMethods(['onExec', 'onClear', 'onSubmit', 'onToggle', 'onSet', 'man']);
+
+            this.events = new atom.Events(this);
+            this.wrapper = this.create('div', 'wrapper', 'body');
+            this.inner = this.create('div', 'inner', this.wrapper);
+            this.output = this.create('div', 'output', this.inner);
+            this.input = this.create('textarea', 'input', this.inner).addEvent({
+                /*========================================
+                =            New code is here            =
+                ========================================*/
+                focus: function() {
+                    makeSuggestions();
+                }
+                /*-----  End of new code  ------*/
+            });
+
+            this.bindKeyboard();
+
+            this.commands = {};
+            this.constructor.commands.forEach(function(command) {
+                this.register(command);
+            }.bind(this));
+
+            this.storage = this.getStorage();
+            this.currentStep = this.storage.length;
+
+            this.input.first.addEventListener("dragenter", Mouse.prevent, false);
+            this.input.first.addEventListener("dragover", Mouse.prevent, false);
+            this.input.first.addEventListener('drop', function(e) {
+                e.stopPropagation();
+                e.preventDefault();
+                this.toggle();
+                Wotg.controller().replays.read(e.dataTransfer.files[0]);
+            }.bind(this), false);
+        },
     });
 });
