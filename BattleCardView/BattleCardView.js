e033cd36ac390b4275bc4ce27fcbcf7ccdf8fcb9
bzz86 <bzz86@mail.ru>
2014-09-11 12:23:48 +0300

    Update BattleCardView.js
    fix

--- BattleCardView/BattleCardView.js
+++ BattleCardView/BattleCardView.js
@@ -25,7 +25,7 @@ new Wotg.Plugins.Simple({
     .move('PackOwn', [  11, -556 ])
     .move('PackOwn.Deck', [  -11, -26 ])
     .move('PackOwn.Grave', [  -11, -26 ])
-    .delete('FlagOwn', [  0, -582 ])
+    .delete('FlagOwn'/*, [  0, -582 ]*/)
     .move('PlatoonsOwn', [  0, 116 ])
     .move('YourTimer', [  0, -355 ])
     .move('ResourcesOwn', [  0, -355 ])
