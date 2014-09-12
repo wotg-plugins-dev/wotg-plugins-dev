dcff123a0692f1a6917a5e3a565301583244a80a
Isk1n <Isk1n@users.noreply.github.com>
2014-09-12 15:35:29 +0300

    Fontsize, textcolor

--- DeckEditor20/DeckEditor20.js
+++ DeckEditor20/DeckEditor20.js
@@ -76,27 +76,27 @@ new Wotg.Plugins.Simple({
     //Art
     Wotg.Card.Markup.MHand.markup.children[0].rect = [8, 5, 55, 55];
     //Title
-    Wotg.Card.Markup.MHand.markup.children[5].rect = [66, 7, 103, 17];
+    Wotg.Card.Markup.MHand.markup.children[5].rect = [66, 6, 103, 17];
     Wotg.Card.Markup.MHand.markup.children[5].textcolor = "rgba(255,255,255,1)";
     //Wotg.Card.Markup.MHand.markup.children[5].fontsize = 14;
     //Count
     Wotg.Card.Markup.MPack.markup.children[11].rect = [150, 40, 20, 25];
     //Power
     Wotg.Card.Markup.MHand.markup.children[10].rect = [65, 34, 23, 23];
-    // Wotg.Card.Markup.MHand.markup.children[10].children[0].fontsize = 14;
-    // Wotg.Card.Markup.MHand.markup.children[10].children[0].textcolor = "rgba(159,143,111,1)";
+    Wotg.Card.Markup.MHand.markup.children[10].children[0].fontsize = 14;
+    Wotg.Card.Markup.MHand.markup.children[10].children[0].textcolor = "rgba(159,143,111,1)";
     // Wotg.Card.Markup.MHand.markup.children[10].children[0].textalign = "left";
     // Wotg.Card.Markup.MHand.markup.children[10].children[0].font = 'Copperplate, Papyrus, fantasy';
     Wotg.Card.Markup.MHand.markup.children[10].children[0].rect = [2, 4, 60, 20];
     //Toughness
     Wotg.Card.Markup.MHand.markup.children[11].rect = [90, 34, 23, 23];
-    // Wotg.Card.Markup.MHand.markup.children[11].children[0].fontsize = 14;
-    // Wotg.Card.Markup.MHand.markup.children[11].children[0].textcolor = "rgba(159,143,111,1)";
+    Wotg.Card.Markup.MHand.markup.children[11].children[0].fontsize = 14;
+    Wotg.Card.Markup.MHand.markup.children[11].children[0].textcolor = "rgba(159,143,111,1)";
     Wotg.Card.Markup.MHand.markup.children[11].children[0].rect = [20, 4, 20, 20];
     //Increase
     Wotg.Card.Markup.MHand.markup.children[8].rect = [115, 34, 23, 23];
-    // Wotg.Card.Markup.MHand.markup.children[8].children[0].fontsize = 14;
-    // Wotg.Card.Markup.MHand.markup.children[8].children[0].textcolor = "rgba(159,143,111,1)";
+    Wotg.Card.Markup.MHand.markup.children[8].children[0].fontsize = 14;
+    Wotg.Card.Markup.MHand.markup.children[8].children[0].textcolor = "rgba(159,143,111,1)";
     Wotg.Card.Markup.MHand.markup.children[8].children[0].rect = [5, 4, 20, 20];
     //Cost
     Wotg.Card.Markup.MHand.markup.children[9].rect = [0, 0, 24, 26];
