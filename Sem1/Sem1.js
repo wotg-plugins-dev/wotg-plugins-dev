784caac82e656bacf1d4acfb783dbeb532fcb26f
bzz86 <bzz86@mail.ru>
2014-09-17 12:19:33 +0200

    img fix

--- Sem1/Sem1.js
+++ Sem1/Sem1.js
@@ -6,7 +6,7 @@ new Wotg.Plugins.Simple({
 
     //новые картинки для индикации атаки и передвижения
     plugin.addImages({
-		'sem1': '/semaphore/semafor1.png'
+		'sem1': 'semaphore/semafor1.png'
 	});
 
     Wotg.Card.Markup.Battle.sprites.moveIndicator = [{
