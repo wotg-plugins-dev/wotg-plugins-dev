6c068b9f5c55daf70ec1ce3ba99093bd45ae6c97
Isk1n <Isk1n@users.noreply.github.com>
2014-09-18 11:50:22 +0300

    Upd VolControl

--- VolControl/VolControl.js
+++ VolControl/VolControl.js
@@ -2,52 +2,94 @@ new Wotg.Plugins.Simple({
     title: 'VolControl',
     version: '0.2.4'
 }, function(plugin, events) {
-
-    function getVolume(key) {
-        var DEFAULT = 0.5;
-        var value = Number(plugin.getConfig(key));
-        if (!value && value !== 0 || value < 0 || value > 1) {
-            value = DEFAULT;
-            plugin.setConfig(key, value);
-        }
-        return value;
-    }
+    var DEFAULT_SOUND_VOLUME = 0.2;
+    var DEFAULT_AMBIENT_VOLUME = 0.1;
+    var DEFAULT_MUSIC_VOLUME = 0.1;
 
     plugin.refactor('Wotg.Utils.Sound', {
-        generateSounds: function(source) {
+        generateSounds: function (source) {
             var target = {};
+            var vol = Number(plugin.getConfig('soundVol'));
 
-            source.forEach(function(name) {
+            source.forEach(function (name) {
                 target[name] = new Howl({
-                    urls: this.urls(name),
-                    buffer: true,
+                    urls    : this.urls(name),
+                    buffer  : false,
                     autoplay: false,
-                    volume: getVolume('vol'),
-                    loop: false
+                    volume  : vol === vol.limit(0,1) ? vol : DEFAULT_SOUND_VOLUME,
+                    loop    : false
                 });
             }.bind(this));
 
             return target;
 
         },
-        music: function(name) {
-            this.stopMusic();
 
-            if (name) {
-                this.currentAudioName = name;
+        ambient: function () {
+            var vol = Number(plugin.getConfig('ambientVol'));
+            if (this.currentAmbientNode) {
+                this.currentAmbientNode.stop();
             }
 
-            if (name && this.on()) {
-                this.currentAudio = new Howl({
-                    urls: this.urls(name),
-                    buffer: true,
-                    autoplay: false,
-                    volume: getVolume('vol'),
-                    loop: true
+            if (this.on()) {
+                if (this.currentAmbientNode) {
+                    this.currentAmbientNode.play();
+                    console.log('- ambient : [ music/ambient ] (cached)');
+                    return;
+                }
+                console.log('- ambient : [ music/ambient ]');
+
+                var audio = this.currentAmbientNode = new Howl({
+                    urls    : this.urls('music/ambient'),
+                    buffer  : false,
+                    autoplay: true,
+                    loop    : false,
+                    volume  : vol === vol.limit(0,1) ? vol : DEFAULT_AMBIENT_VOLUME,
+                    onend   : function () {
+                        audio = null;
+                        this.ambient();
+                    }.bind(this)
                 });
-
-                this.currentAudio.fadeIn(1, this.fadeTime);
             }
+        },
+
+        playNextMusic: function () {
+            var vol = Number(plugin.getConfig('musicVol'));
+            console.log('playNext');
+
+            var name = this.currentMusic, cached;
+
+            if (!this.on()) {
+                return;
+            }
+
+            if (name && name.getNextId) {
+                name = name.getNextId();
+            }
+
+            if (!name) {
+                return;
+            }
+
+            cached = this.musicCache[name];
+
+            if (cached) {
+                console.log('- music   : [ ' + name + ' ] (cached)');
+                this.currentAudioNode = cached;
+                cached.play();
+                return;
+            }
+
+            console.log('- music   : [ ' + name + ' ]');
+
+            this.musicCache[name] = this.currentAudioNode = new Howl({
+                urls    : this.urls(name),
+                buffer  : false,
+                autoplay: true,
+                loop    : false,
+                volume  : vol === vol.limit(0,1) ? vol : DEFAULT_MUSIC_VOLUME,
+                onend   : this.playNextMusic
+            });
         }
     });
 
