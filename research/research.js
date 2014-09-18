b4ee7d0d23bff8bffcb5d30a5498e5efb444e307
J-S-K <kiruj@yandex.ru>
2014-09-18 14:48:59 +0300

    Update research.js

--- research/research.js
+++ research/research.js
@@ -261,7 +261,7 @@ function jslog(text) {
 			return (Wotg.controller().protos.get(a.id).level - Wotg.controller().protos.get(b.id).level)
 		});
 		for (var i = 0 ; i < list.length; i++) { //list.length
-			list[i].slot = i;
+			list[i].slotX = i;
 		//	this.createHqCard(list[i]);
 			var elem = new Wotg.Research.AllCardItem(this.app.layer, {
 			manager: this,
@@ -289,8 +289,8 @@ function jslog(text) {
 		var columns = 12,
 		width = 127,
 		hight = 127,
-		x = this.slot % columns,
-		y= (this.slot-x) / columns;
+		x = this.slotX % columns,
+		y= (this.slotX-x) / columns;
 		return new Point (x*width,y*hight+40);
 	}
     });
